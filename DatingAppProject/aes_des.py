bdc57ef172b56dbbb70cd6245309319a06cd81df
anishbista92 <anishbista9235@gmail.com>
2024-09-25 22:31:28 +0545

    add: location based search

--- DatingAppProject/aes_des.py
+++ DatingAppProject/aes_des.py
@@ -4,69 +4,52 @@ from cryptography.hazmat.primitives import padding
 from os import urandom
 import hashlib
 
-AES_BLOCK_SIZE = 128  # AES block size is 128 bits
+AES_BLOCK_SIZE = 128
 
 
 def pad_message(message: str) -> bytes:
-    # Padding the message to ensure it is a multiple of the block size
+
     padder = padding.PKCS7(AES_BLOCK_SIZE).padder()
     padded_data = padder.update(message.encode()) + padder.finalize()
     return padded_data
 
 
 def unpad_message(padded_message: bytes) -> str:
-    # Unpadding the decrypted message
+
     unpadder = padding.PKCS7(AES_BLOCK_SIZE).unpadder()
     data = unpadder.update(padded_message) + unpadder.finalize()
     return data.decode()
 
 
 def encrypt_message(message: str, shared_key: str) -> str:
-    # Convert shared key to a valid hexadecimal string using SHA-256
-    key = hashlib.sha256(shared_key.encode()).hexdigest()[:32]
-    key = bytes.fromhex(key)  # AES-128 uses 16 bytes (32 hex characters)
-    iv = urandom(16)  # AES requires a 16-byte initialization vector (IV)
 
-    # Pad the message before encryption
+    key = hashlib.sha256(shared_key.encode()).hexdigest()[:32]
+    key = bytes.fromhex(key)
+    iv = urandom(16)
+
     padded_message = pad_message(message)
 
-    # Create AES cipher in CBC mode
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     encryptor = cipher.encryptor()
 
-    # Encrypt the message
     encrypted_message = encryptor.update(padded_message) + encryptor.finalize()
 
-    # Return the IV + encrypted message in hex format
     return (iv + encrypted_message).hex()
 
 
 def decrypt_message(encrypted_message_hex: str, shared_key: str) -> str:
-    # Convert the encrypted message and shared key to bytes
+
     encrypted_message = bytes.fromhex(encrypted_message_hex)
     key = hashlib.sha256(shared_key.encode()).hexdigest()[:32]
-    key = bytes.fromhex(key)  # AES-128 uses 16 bytes (32 hex characters)
-    iv = encrypted_message[:16]  # First 16 bytes are the IV
-    encrypted_message = encrypted_message[
-        16:
-    ]  # Remaining bytes are the actual ciphertext
+    key = bytes.fromhex(key)
+    iv = encrypted_message[:16]
+    encrypted_message = encrypted_message[16:]
 
-    # Create AES cipher in CBC mode
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     decryptor = cipher.decryptor()
 
-    # Decrypt the message
     decrypted_padded_message = (
         decryptor.update(encrypted_message) + decryptor.finalize()
     )
 
-    # Unpad the decrypted message and return it
     return unpad_message(decrypted_padded_message)
-
-
-message = "WE\."
-shared_key = "65c0ac8d12081cd97d17b02193417965d8b1a54941ed76f7cd67b9802ab7272c"
-encrypted_message = encrypt_message(message, shared_key)
-print(f"Encrypted: {encrypted_message}")
-decrypted_message = decrypt_message(encrypted_message, shared_key)
-print(f"Decrypted: {decrypted_message}")
--- find_users/urls.py
+++ find_users/urls.py
@@ -4,5 +4,5 @@ from find_users.views import FilterUser, get_users_by_raduis
 
 urlpatterns = [
     path("users/", FilterUser, name="filter_user"),
-    path("by_radius/", get_users_by_raduis),
+    path("by_radius/", get_users_by_raduis, name="get_users_by_radius"),
 ]
